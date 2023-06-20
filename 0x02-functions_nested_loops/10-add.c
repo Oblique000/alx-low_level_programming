c2b1a7f1ba068aa988d1b956889a5ece0c7d9b43
Oblique000 <neomathe000@gmail.com>
2023-06-20 13:24:35 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the results
+ * @a: Integer to be added to integer b
+ * @b: Integer to be added to integer a
+ * Return: The sum of integer a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
