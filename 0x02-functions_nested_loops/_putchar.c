bdac679dccb18403bf8efeaae191158231f0dc50
Oblique000 <neomathe000@gmail.com>
2023-06-20 07:23:16 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
-#include "main.h"
 #include <unistd.h>
+
 /**
- * _putchar - Write character c to stdout
- * @c: Character to print
+ * _putchar - Writes the character c to stdout
+ * @c: The character to print
  *
- * Return: success 1
- * on error, -1 is returned, and set error appropriately
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately
  */
 int _putchar(char c)
 {
-	return (write(1, && c, 1));
+	return (write(1, &c, 1));
 }
