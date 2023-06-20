f3524a03faedf1ba6dc6423e354e00a8ef89a79e
Oblique000 <neomathe000@gmail.com>
2023-06-20 14:58:51 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all multiples of 3 or 5 up to 1024
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x, y = 0;
+
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			y += x;
+		}
+		y++;
+	}
+	print("%d\n", y);
+	return (0);
+}
