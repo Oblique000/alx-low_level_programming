b3b1f44c4fc774377f024eb7afba8514367b0e57
Oblique000 <neomathe000@gmail.com>
2023-06-21 07:08:18 -0700

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,5 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int day, int month, int year);
-int convert_day(int day, int month);
 
-#endif
+#endif /* MAIN_H */
