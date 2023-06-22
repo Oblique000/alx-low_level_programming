0b4cdebff43016e51c8726f3c436f497dd90d9f4
Oblique000 <neomathe000@gmail.com>
2023-06-22 05:12:57 -0700

    Collaboration of multiplicationi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Addition of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
