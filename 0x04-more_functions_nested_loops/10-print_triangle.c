91a71b5bf12ecda8bd40401754f968a1e421a845
Oblique000 <neomathe000@gmail.com>
2023-06-22 09:29:18 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,24 +8,24 @@
 
 void print_triangle(int size)
 {
+	int x, y;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int x, y;
-
 		for (x = 1; x <= size; x++)
 		{
 			for (y = x; y <= size; y++)
 			{
-				_putchar(' ');
+				_putchar(32);
 			}
 
-			for (y = 1; y <= size; y++)
+			for (y = 1; y <= x; y++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
 
 			_putchar('\n');
