dcbb17f2ade1f2af3d772e9c76b60d8f9dd4aee3
Oblique000 <neomathe000@gmail.com>
2023-06-22 04:55:24 -0700

    Is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if numbers are 0 - 9
+ * @x: char to be checked
+ *
+ * Return: 1 for char that will be a digit or 0 for anything else
+ */
+
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
