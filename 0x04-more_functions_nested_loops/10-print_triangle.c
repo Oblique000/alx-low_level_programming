3a41407c90d3327c34b00f39b20ec03f00994395
Oblique000 <neomathe000@gmail.com>
2023-06-22 09:11:36 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle, followed by a new line
+ * @size: The size of triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x, y;
+
+		for (x = 1; x <= size; x++)
+		{
+			for (y = x; y <= size; y++)
+			{
+				_putchar(' ');
+			}
+			for (y = 1; y <= size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
