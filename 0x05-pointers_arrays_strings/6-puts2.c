8b8921a7c6421108fd23428621df35c257a5cccb
Oblique000 <neomathe000@gmail.com>
2023-06-27 03:09:18 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,24 +7,15 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
 	int k = 0;
-	char *y = str;
-	int l;
 
-	while (*y != '\0')
+	while (str[k] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	k = longi - 1;
-
-	for (l = 0; l <= k; l++)
-	{
-		if (l % 2 == 0)
+		if (k % 2 == 0)
 		{
-			_putchar(str[0]);
+			_putchar(str[k]);
 		}
+		k++;
 	}
 	_putchar('\n');
 }
