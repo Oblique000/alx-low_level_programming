86e44b58ed1161647f06388f0d6c4db1ce362942
Oblique000 <neomathe000@gmail.com>
2023-06-30 09:00:19 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,19 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x;
+	int index = 0, src_len = 0;
 
-	x = 0;
-	while (x < n && src[x] != '\0')
+	while (src[index++])
 	{
-		dest[x] = src[x];
-			x++;
+		src_len++;
+	}
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (index = src_len; index < n; index++)
+	{
+		dest[index] = '\0';
 	}
 	return (dest);
 }
