3d430629b438479e2d3034fc8c30319ecd4de2b5
Oblique000 <neomathe000@gmail.com>
2023-06-30 07:57:11 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function that copies a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	x = 0;
+	while (x < n && src[x] != '\0')
+	{
+		dest[x] = src[x];
+			x++;
+	}
+	return (dest);
+}
