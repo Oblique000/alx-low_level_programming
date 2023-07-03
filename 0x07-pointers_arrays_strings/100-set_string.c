abd03775f8200627dfab49bbaef000a9018271bf
Oblique000 <neomathe000@gmail.com>
2023-07-03 10:09:14 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - A function that sets a value
+ * @s: An input
+ * @to: An input
+ * Return: Always 0 (success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
