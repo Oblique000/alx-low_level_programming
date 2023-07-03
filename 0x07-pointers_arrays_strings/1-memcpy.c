66c10f1713fae16ae5fb346758180df20f639027
Oblique000 <neomathe000@gmail.com>
2023-07-03 08:35:11 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - A funnction that copies memory area
+ * @dest: Memory stored
+ * @src: Memory is copied
+ * @n: The number of bytes
+ * Return: copied memory with n bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x = 0;
+	int y = 0;
+
+	for (; x < y; x++)
+	{
+		dest[x] = src[x];
+		n--;
+	}
+	return (dest);
+}
