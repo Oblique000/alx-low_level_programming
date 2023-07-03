6ca565bac792fa80c9f496e0b39e44685cc97071
Oblique000 <neomathe000@gmail.com>
2023-07-03 09:31:00 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - A function that locates a substring
+ * @haystack: An input
+ * @needle: An input
+ * Return: Always 0 (success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *x = haystack;
+		char *y = needle;
+
+		while (*x == *y && *y != '\0')
+		{
+			x++;
+			y++;
+		}
+		if (*y == '\0')
+			return (haystack);
+	}
+	return (0);
+}
