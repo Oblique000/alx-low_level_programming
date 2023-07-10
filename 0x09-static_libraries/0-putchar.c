537d8a29658c8254956a8073d9a7862add0e895a
Oblique000 <neomathe000@gmail.com>
2023-07-10 13:28:31 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes char c to stdout
+ * _putchar - Write char c to stdout
  * @c: Char to print
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately
@@ -9,5 +9,5 @@
 
 int _putchar(char c)
 {
-	return(writes(1, &c, 1));
+	return(write(1, &c, 1));
 }
