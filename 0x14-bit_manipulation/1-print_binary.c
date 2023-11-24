9af00d870bbe618a15873eb3bb5f9d1b5630b245
Oblique000 <neomathe000@gmail.com>
2023-11-23 18:13:53 -0800

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,49 @@
 #include "main.h"
 
 /**
- * print_binary - function that prints binary num equivalent to dec num
+ * _pow - func that calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int x;
+
+	num = 1;
+	for (x = 1; x <= power; x++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - function that prints a num in binary notation
  * @n: num to print
- * Return: Always 0 (success)
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	int x, count = 0;
-	unsigned long int current;
+	unsigned long int current, check;
+	char a;
 
-	for (x = 63; x >= 0; x++)
+	a = 0;
+	current = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (current != 0)
 	{
-		current = n >> x;
-
-		if (current & x)
+		check = n & current;
+		if (check == current)
 		{
+			a = 1;
 			_putchar('1');
-			count++;
 		}
-		while (count)
+		else if (a == 1 || current == 1)
+		{
 			_putchar('0');
+		}
+		current >>= 1;
 	}
-	if (!count)
-		_putchar('0');
 }
+
