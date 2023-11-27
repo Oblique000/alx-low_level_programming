b54583e7e357dd6cea05348249bbe9e3934f35d7
Oblique000 <neomathe000@gmail.com>
2023-11-27 14:45:23 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,23 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int x, y, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];) len++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
+	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	y = write(fd, text_content, len);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-
-	if (fd == -1 || w == -1)
+	if (x == -1 || y == -1)
 		return (-1);
 
-	close(fd);
+	close(x);
 
 	return (1);
 }
